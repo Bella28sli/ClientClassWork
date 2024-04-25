a0956578ea8a8a5a028987bc4462e34e2ef6bbe0
Diana <izabellaslivina@gmail.com>
2024-04-25 09:40:58 +0300

    FuckSociety

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClassWork25042024Ver2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Socket mySocket;
+        public MainWindow()
+        {
+            InitializeComponent();
+            mySocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            mySocket.Connect("26.80.234.197", 4545);
+
+            ReceiveMessage();
+        }
+
+        private async Task SendMessage(string message)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(message);
+            await mySocket.SendAsync(bytes, SocketFlags.None);
+        }
+
+        private void SendButton_Click(object sender, RoutedEventArgs e)
+        {
+            SendMessage(MessageText.Text);
+        }
+
+        private async Task ReceiveMessage()
+        {
+            while (true)
+            {
+                byte[] bytes = new byte[1024];
+                await mySocket.ReceiveAsync(bytes, SocketFlags.None);
+                string message = Encoding.UTF8.GetString(bytes);
+                Messages.Items.Add(message);
+            }
+        }
+    }
+}
